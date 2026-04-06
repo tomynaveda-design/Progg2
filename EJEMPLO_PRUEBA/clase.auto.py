877e01bf9d082a104ce309536a04595fd6467fca
tomynaveda-design <tomynaveda@gmail.com>
2026-04-06 18:01:46 -0300

    NUEVO ARCHIVO

--- EJEMPLO_PRUEBA/clase.auto.py
+++ EJEMPLO_PRUEBA/clase.auto.py
@@ -1,7 +1,11 @@
+class Vehiculo:
+    def __init__(self, patente, tipo):
+        self.patente = patente
+        self.tipo = tipo  # Puede ser "Automovil" o "Camioneta"
+        self.tarifa = 1500 if tipo == "Automovil" else 2200
 
-
-
-
+    def mostrar_datos(self):
+        return f"Vehículo [{self.patente}] - Tipo: {self.tipo} - Tarifa/h: ${self.tarifa}"
 
 
 
